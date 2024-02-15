927d1a01620ef3a640ccc6505df2d3fa57df4034
youssef-ait-elmahi <youssefaitelmahi3@gmail.com>
2024-02-15 17:09:17 +0100

    Refactor starwarsCharacters function to use async/await

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,42 +1,21 @@
 #!/usr/bin/node
-const request = require('request');
+// starwars_characters - prints all characters of a Star Wars movie by its id.
+const util = require('util');
+const request = util.promisify(require('request'));
+const filmID = process.argv[2];
 
-const fetchCharacter = (url) => {
-  return new Promise((resolve, reject) => {
-    request.get(url, (error, response, body) => {
-      if (error) {
-        reject(error);
-      } else {
-        resolve(JSON.parse(body).name);
-      }
-    });
-  });
-};
+async function starwarsCharacters (filmId) {
+  const endpoint = 'https://swapi-api.hbtn.io/api/films/' + filmId;
+  let response = await (await request(endpoint)).body;
+  response = JSON.parse(response);
+  const characters = response.characters;
 
-const fetchFilm = (id) => {
-  return new Promise((resolve, reject) => {
-    const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
-    request.get(url, (error, response, body) => {
-      if (error) {
-        reject(error);
-      } else {
-        resolve(JSON.parse(body).characters);
-      }
-    });
-  });
-};
-
-const printCharacters = async () => {
-  try {
-    const id = process.argv[2];
-    const charactersUrls = await fetchFilm(id);
-
-    const characterNames = await Promise.all(charactersUrls.map(fetchCharacter));
-
-    characterNames.forEach(name => console.log(name));
-  } catch (error) {
-    console.error(error);
+  for (let i = 0; i < characters.length; i++) {
+    const urlCharacter = characters[i];
+    let character = await (await request(urlCharacter)).body;
+    character = JSON.parse(character);
+    console.log(character.name);
   }
-};
+}
 
-printCharacters();
+starwarsCharacters(filmID);
