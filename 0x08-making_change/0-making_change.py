70941f0760b0f02b679997d1826f1f4cb649973a
youssef-ait-elmahi <youssefaitelmahi3@gmail.com>
2024-02-27 18:04:09 +0100

    Refactor makeChange function to improve efficiency

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -5,14 +5,11 @@ def makeChange(coins, total):
     if total <= 0:
         return 0
 
-    else:
-        dp = [float('inf')] * (total + 1)
-        dp[0] = 0
+    dp = [float('inf')] * (total + 1)
+    dp[0] = 0
 
-        """Build the dp table"""
-        for i in range(len(coins)):
-            for j in range(coins[i], total + 1):
-                dp[j] = min(dp[j], dp[j - coins[i]] + 1)
+    for i in range(len(coins)):
+        for j in range(coins[i], total + 1):
+            dp[j] = min(dp[j], dp[j - coins[i]] + 1)
 
-        """Return the result"""
-        return dp[total] if dp[total] != float('inf') else -1
+    return dp[total] if dp[total] != float('inf') else -1
