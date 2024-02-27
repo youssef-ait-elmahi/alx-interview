954471d6437f0c461aaed3d0b6b654c91da20c5d
youssef-ait-elmahi <youssefaitelmahi3@gmail.com>
2024-02-27 18:05:18 +0100

    Refactor makeChange function to improve efficiency

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
+"""Making change"""
+
+
 def makeChange(coins, total):
     """making change for a given total using the fewest number of coins"""
-
     if total <= 0:
         return 0
 
     dp = [float('inf')] * (total + 1)
     dp[0] = 0
 
-    for i in range(len(coins)):
-        for j in range(coins[i], total + 1):
-            dp[j] = min(dp[j], dp[j - coins[i]] + 1)
+    for i in range(1, total + 1):
+        for coin in coins:
+            if i >= coin:
+                dp[i] = min(dp[i], dp[i - coin] + 1)
 
     return dp[total] if dp[total] != float('inf') else -1
