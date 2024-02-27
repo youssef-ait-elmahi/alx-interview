01ccb167621afc49cca7b58a8766b0b9a5bddef1
youssef-ait-elmahi <youssefaitelmahi3@gmail.com>
2024-02-27 17:56:06 +0100

    Refactor makeChange function to handle edge case

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
 def makeChange(coins, total):
     """making change for a given total using the fewest number of coins"""
-    dp = [float('inf')] * (total + 1)
-    dp[0] = 0
 
-    # Build the dp table
-    for i in range(len(coins)):
-        for j in range(coins[i], total + 1):
-            dp[j] = min(dp[j], dp[j - coins[i]] + 1)
+    if total <= 0:
+        return 0
+    
+    else:
+        dp = [float('inf')] * (total + 1)
+        dp[0] = 0
 
-    # Return the result
-    return dp[total] if dp[total] != float('inf') else -1
+        # Build the dp table
+        for i in range(len(coins)):
+            for j in range(coins[i], total + 1):
+                dp[j] = min(dp[j], dp[j - coins[i]] + 1)
+
+        # Return the result
+        return dp[total] if dp[total] != float('inf') else -1
