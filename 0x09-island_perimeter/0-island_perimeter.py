68ee5f6313a30400f2dab14bd0d375cc9feadcf6
youssef-ait-elmahi <youssefaitelmahi3@gmail.com>
2024-03-06 15:20:51 +0100

    Refactor island_perimeter function to improve efficiency

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,35 +1,17 @@
 #!/usr/bin/python3
+""" Island perimeter """
+
+
 def island_perimeter(grid):
-    """
-    Calculate the perimeter of an island in a grid.
-
-    Args:
-        grid (List[List[int]]): A 2D list of integers where 0 represents
-        water and 1 represents land.
-
-    Returns:
-        int: The perimeter of the island.
-
-    This function works by iterating over each cell in the grid. If the cell
-    is land (1),it checks the four directions (up, down, left, right). If any
-    of these directions is either water (0) or out of the grid boundaries, it
-    adds 1 to the perimeter count.
-    """
-    rows = len(grid)
-    cols = len(grid[0])
-
-    result = 0
-
-    for r in range(rows):
-        for c in range(cols):
-            if grid[r][c] == 1:
-                if r == 0 or grid[r-1][c] == 0:
-                    result += 1
-                if r == rows-1 or grid[r+1][c] == 0:
-                    result += 1
-                if c == 0 or grid[r][c-1] == 0:
-                    result += 1
-                if c == cols-1 or grid[r][c+1] == 0:
-                    result += 1
-
-    return result
+    """ define island_perimeter """
+    row, column = len(grid), len(grid[0])
+    perimeter = 0
+    for i in range(row):
+        for j in range(column):
+            if grid[i][j] == 1:
+                perimeter += 4
+                if i > 0 and grid[i-1][j] == 1:
+                    perimeter -= 2
+                if j > 0 and grid[i][j-1] == 1:
+                    perimeter -= 2
+    return perimeter
